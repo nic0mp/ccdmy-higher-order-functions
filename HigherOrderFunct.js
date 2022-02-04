3234b9cddd5e0db2375a8a8db71e1c29f343c6b8
nic0mp <nico.mp215@gmail.com>
2022-02-04 16:16:24 -0600

    .forEach method

--- HigherOrderFunct.js
+++ HigherOrderFunct.js
@@ -11,3 +11,16 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
   is2p2()
   console.log(is2p2.name)
+
+//   ITERATORS
+//  .forEach() method
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(produceItem =>
+console.log(`I want to eat a ${produceItem}`))
+// output:
+// I want to eat a mango
+// I want to eat a papaya
+// I want to eat a pineapple
+// I want to eat a apple
